b591374a89757ad81089c5abec76ccef2123d8e9
Jordan Humphrey <jphumphrey2@gmail.com>
2019-08-04 11:44:29 -0700

    single, master, and barrier commands

--- OpenMp.cpp
+++ OpenMp.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdio>
 #include <chrono>
 using namespace std;
 
@@ -15,9 +16,69 @@ void hello_openmp()
 	}
 }
 
+void pfor()
+{
+	const int length = 1024 * 1024 * 64;
+	float *a = new float[length],
+		*b = new float[length],
+		*c = new float[length],
+		*result = new float[length];
+
+#pragma omp parallel for
+	for (int i = 0; i < length; i++)
+	{
+		result[i] = a[i] + b[i] * erff(c[i]);
+	}
+
+	delete[] a;
+	delete[] b;
+	delete[] c;
+	delete[] result;
+}
+
+void sections()
+{
+#pragma omp parallel sections
+	{
+#pragma omp section
+		{
+			for (int i = 0; i < 1000; i++)
+			{
+				cout << i;
+			}
+		}
+#pragma omp section
+		{
+			for (int i = 0; i < 1000; i++)
+			{
+				cout << static_cast<char>('a' + (i % 26));
+			}
+		}
+	}
+}
+
+void single_master()
+{
+#pragma omp parallel
+	{
+#pragma omp single
+		printf("gathering input: %d\n", omp_get_thread_num());
+
+		printf("in parallel on %d\n", omp_get_thread_num());
+
+#pragma omp barrier
+
+#pragma omp master
+		printf("output on: %d\n", omp_get_thread_num());
+	}
+}
+
 int main(int argc, char* argv[])
 {
-	hello_openmp();
-	getchar();
+	//hello_openmp();
+	//pfor();
+	//sections();
+	single_master();
+
 	return 0;
 }
